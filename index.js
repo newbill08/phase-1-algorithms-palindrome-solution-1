a8dd31a09ed8ada4a7a4bd2dcaf7d2fd32e4e9ae
joshua <josh.newbill08@hotmail.com>
2023-06-03 23:37:20 -0500

    Palindrome using split method

--- index.js
+++ index.js
@@ -1,9 +1,30 @@
 function isPalindrome(word) {
   // Write your algorithm here
-}
+    /*
+    take a word and reverse it and test if it is the same backwards
+    
+    */
 
+    const wordReversed = word.split("").reverse().join("")
+    console.log(wordReversed)
+   return testValues(wordReversed, word)
+
+    
+}
+function testValues(ReverseWord,word){
+  if (ReverseWord != word ){
+    return false
+  }
+  else{
+    return true
+  }
+
+
+}
 /* 
   Add your pseudocode here
+    Turn word into an array 
+    If array not equal to the reverse then return false
 */
 
 /*
@@ -15,6 +36,24 @@ if (require.main === module) {
   // add your own custom tests in here
   console.log("Expecting: true");
   console.log("=>", isPalindrome("racecar"));
+
+  console.log("");
+
+  console.log("Expecting: false");
+  console.log("=>", isPalindrome("robot"));
+
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome("abba"));
+
+  console.log("");
+
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome("a"));
+
+  console.log("");
+
+  console.log("Expecting: false");
+  console.log("=>", isPalindrome("hi"));
 
   console.log("");
 
